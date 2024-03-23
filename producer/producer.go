deecf6c4b765ca87e82140ebb4dc9143a6813c03
Pranav Nallari <61450519+pranavnallari@users.noreply.github.com>
2024-03-23 14:55:06 +0530

    Initial Commit

---
+++ producer/producer.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":8080")
+}
+
+func ConnectProducer(brokersURL []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersURL, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, err
+}
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersURL := []string{"localhost:29092"}
+	producer, err := ConnectProducer(brokersURL)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in Topic : %s/partition : %d/offset : %d\n", topic, partition, offset)
+	return nil
+}
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Printf("Error occurred : %v", err.Error())
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err.Error(),
+		})
+		return err
+	}
+	cmtInBytes, err := json.Marshal(cmt)
+	if err != nil {
+		log.Printf("Error occurred : %v", err.Error())
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err.Error(),
+		})
+		return err
+	}
+	PushCommentToQueue("comments", cmtInBytes)
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": cmt,
+	})
+	if err != nil {
+		log.Printf("Error occurred : %v", err.Error())
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "error creating product",
+		})
+		return err
+	}
+	return err
+}
